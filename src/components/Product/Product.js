34a2b01dc984a339d487cb3c2caebb7d40885987
Md Abbas Ali <mdabbasali185@gmail.com>
2022-03-24 02:10:34 +0600

    card section complete

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,15 +1,30 @@
-import React from 'react';
-import './Product.css'
+import React from "react";
+import "./Product.css";
 
 const Product = (props) => {
-        const {name,img,seller,price,ratings}=props.product
-    return (
-        <div className='product'>
-            <img src={img} alt="" />
-            <p className='product-name'>{name}</p>
-            <button className='add-to-cart'>Add to Cart</button>
-        </div>
-    );
+  const { name, img, seller, price, ratings } = props.product;
+  return (
+    <div className="product">
+      <img src={img} alt="" />
+      <div className="product-info">
+      <p className="product-name">
+        <span>Name: </span> {name}
+      </p>
+      <p>
+        <span> Manufacturer: </span>
+        {seller}
+      </p>
+      <p>
+        <span> Price: $</span>
+        {price}
+      </p>
+      <p>
+        <span>Ratings: </span> {ratings} star
+      </p>
+      </div>
+      <button className="add-to-cart">Add to Cart</button>
+    </div>
+  );
 };
 
 export default Product;
