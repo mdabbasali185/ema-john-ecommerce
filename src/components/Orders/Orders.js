6e574b442b9defe039a18c88217841f0e3fea270
Md Abbas Ali <mdabbasali185@gmail.com>
2022-04-10 01:16:22 +0600

    custom hooks

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React from "react";
+import useCart from "../../hooks/useCart";
+import useProduct from "../../hooks/useProduct";
 
 const Orders = () => {
-    return (
-        <div>
-            <h2>THis is</h2>
-        </div>
-    );
+  const [products, setProducts] = useProduct();
+  const [cart,setCart]=useCart(products)
+  return (
+    <div>
+      <h2>THis is orders:{products.length}</h2>
+      <h3>Cart has : {cart.length}</h3>
+    </div>
+  );
 };
 
 export default Orders;
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,17 +1,14 @@
 import React, { useEffect, useState } from "react";
 import Cart from "../../Cart/Cart";
+import useProduct from "../../hooks/useProduct";
 import { addToDb, getStoredCart } from "../../utilities/fakedb";
 import Product from "../Product/Product";
 import "./Shop.css";
 
 const Shop = () => {
-  const [products, setProducts] = useState([]);
+  const [products, setProducts] = useProduct();
   const [cart, setCart] = useState([]);
-  useEffect(() => {
-    fetch("products.json")
-      .then((res) => res.json())
-      .then((data) => setProducts(data));
-  }, []);
+
   useEffect(() => {
     const storedCart = getStoredCart();
     const savedCart = [];
