8676fedc05396925ddd39d00148f9d1b46ea346c
Md Abbas Ali <mdabbasali185@gmail.com>
2022-04-10 01:36:36 +0600

    single item card select

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,14 +1,22 @@
 import React from "react";
+import Cart from "../../Cart/Cart";
 import useCart from "../../hooks/useCart";
 import useProduct from "../../hooks/useProduct";
+import ReviewItem from "../ReviewItem/ReviewItem";
 
 const Orders = () => {
   const [products, setProducts] = useProduct();
-  const [cart,setCart]=useCart(products)
+  const [cart, setCart] = useCart(products);
   return (
-    <div>
-      <h2>THis is orders:{products.length}</h2>
-      <h3>Cart has : {cart.length}</h3>
+    <div className="shop-container">
+      <div className="products-container">
+        {cart.map((product) => (
+          <ReviewItem key={product.id} product={product}></ReviewItem>
+        ))}
+      </div>
+      <div className="cart-container">
+        <Cart cart={cart}></Cart>
+      </div>
     </div>
   );
 };
