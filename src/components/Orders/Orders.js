79c8c5f1228aa1b366492596aab3a2f7df62210d
Md Abbas Ali <mdabbasali185@gmail.com>
2022-04-10 12:53:13 +0600

    fontawsome add

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -3,15 +3,24 @@ import Cart from "../../Cart/Cart";
 import useCart from "../../hooks/useCart";
 import useProduct from "../../hooks/useProduct";
 import ReviewItem from "../ReviewItem/ReviewItem";
+import "./Orders.css";
 
 const Orders = () => {
   const [products, setProducts] = useProduct();
   const [cart, setCart] = useCart(products);
+  const handleRemoveProduct = (product) => {
+    const rest = cart.filter((pd) => pd.id !== product.id);
+    setCart(rest);
+  };
   return (
     <div className="shop-container">
-      <div className="products-container">
+      <div className="review-items-container">
         {cart.map((product) => (
-          <ReviewItem key={product.id} product={product}></ReviewItem>
+          <ReviewItem
+            key={product.id}
+            product={product}
+            handleRemoveProduct={handleRemoveProduct}
+          ></ReviewItem>
         ))}
       </div>
       <div className="cart-container">
--- src/components/ReviewItem/ReviewItem.js
+++ src/components/ReviewItem/ReviewItem.js
@@ -1,10 +1,43 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
+import "./ReviewItem.css";
 
 const ReviewItem = (props) => {
-  const { name, price, shipping, quantity } = props.product;
+  const { product, handleRemoveProduct } = props;
+  const { name, price, img, shipping, quantity } = props.product;
   return (
-    <div>
-      <h3>This is {name}</h3>
+    <div className="review-item">
+      <div>
+        <img src={img} alt="" />
+      </div>
+      <div className="review-item-details-container">
+        <div className="review-item-details">
+          <p className="product-name" title={name}>
+            {name.length > 20 ? name.slice(0, 20) + "..." : name}
+          </p>
+          <p>
+            price: <span className="orange-color"> ${price}</span>
+          </p>
+          <p>
+            <small>Shipping: ${shipping}</small>
+          </p>
+          <p>
+            <small>Quantity:{quantity}</small>
+          </p>
+        </div>
+        <div className="delete-container">
+          <button
+            onClick={() => handleRemoveProduct(product)}
+            className="delete-button"
+          >
+            <FontAwesomeIcon
+              className="delete-icon"
+              icon={faTrashAlt}
+            ></FontAwesomeIcon>
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
